f8e0eaa372e0d7da5d76d31968bd757d4950dc3c
AndreiFomin1isd <1501afomin@gmail.com>
2026-03-12 08:52:00 +0300

    Delete praktika_srez_znani directory

--- praktika_srez_znani/task_srez_10.py
+++
@@ -1,9 +0,0 @@
-N1 = int(input("Введите первое число: "))
-N2 = int(input("Введите второе число: "))
-
-if N1 > N2:
-    print(N1)
-elif N1 < N2:
-    print(N2)
-elif N1 == N2:
-    print(N1)
--- praktika_srez_znani/task_srez_18.py
+++
@@ -1,8 +0,0 @@
-X = float(input("Введите X: "))
-
-if X>0:
-    res = X % 1
-    print(res)
-
-if X<0:
-    print("Число отрицательное")
